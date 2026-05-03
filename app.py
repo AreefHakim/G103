d7bd26690aea42c63927605be5431173a37e1a9c
Qayyim Zafir <qayyimzafir4@gmail.com>
2026-05-03 20:53:30 +0800

    Update UI and store registration page

--- app.py
+++ app.py
@@ -101,5 +101,10 @@ def register():
     return render_template('register.html', form=form)
 
 
+@app.route('/forgot-password')
+def forgotpassword():
+    return render_template('forgotpassword.html')
+
+
 if __name__ == "__main__":
     app.run(debug=True)
